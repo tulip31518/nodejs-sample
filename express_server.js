ac5ca441f14c76d46be52325e0e3ae086a14b9d0
tulip <tulip31518@outlook.com>
2018-08-11 19:09:53 +0800

    file upload

--- express_server.js
+++ express_server.js
@@ -1,13 +1,85 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-});
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(multer({ dest: '/tmp/'}).single('file'));
+var uploads = multer({dest: './tmp'});
+
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.post('/file_upload',uploads.single('file'), function (req, res) {
+    console.log(req.file.name);
+    console.log(req.file.path);
+    console.log(req.file.type);
+    var file = __dirname + "/tmp/" + req.file.name + ".jpg";
+    
+    fs.readFile( req.file.path, function (err, data) {
+       fs.writeFile(file, data, function (err) {
+          if( err ){
+             console.log( err );
+             }else{
+                response = {
+                   message:'File uploaded successfully',
+                   filename:req.file.name
+                };
+             }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+    });
+ })
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+ })
 
 var server = app.listen(8081, function () {
+
    var host = server.address().address
    var port = server.address().port
-   
+
    console.log("Example app listening at http://%s:%s", host, port)
-});
+})
